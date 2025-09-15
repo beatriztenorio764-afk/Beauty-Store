601020417c838ebd19cc13a46991dd3cc92c2993
tiendaTecno <149434850+tiendaTecno@users.noreply.github.com>
2025-09-14 19:32:59 -0600

    initial commit

--- js/CarritoCompras.js
+++ js/CarritoCompras.js
@@ -31,7 +31,7 @@ let sumtotal=0;
 function creacarritobtnwhatsapp(){//crea boton de Whatsapp
 let newcarrimsg=document.createElement("a");
 let newcarriimg=document.createElement("img");
-let numertel=+505781776694;
+let numertel=+50581776694;
 
 newcarrimsg.setAttribute("aria-label","Chat on WhatsApp");
 newcarrimsg.setAttribute("href", "https://wa.me/"+numertel+"/?text='"+ fmsgpedido() +"'");
