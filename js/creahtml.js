cc31fd950b71dd9a958c1dda9b991c8a44dcbd9d
tiendaTecno <149434850+tiendaTecno@users.noreply.github.com>
2025-09-24 12:35:45 -0600

    initial commit

--- js/creahtml.js
+++ js/creahtml.js
@@ -379,12 +379,13 @@ function Btcarritoc(idbtcompra, i, newDiv) {
   let newDivbtn = document.createElement("div");
   let newbtn = document.createElement("button");
 
-  newDivbtn.setAttribute("class", "Titulo");
+  newDivbtn.setAttribute("class", "divinputnum");
   newbtn.setAttribute("id", idbtcompra);
   newbtn.setAttribute("value", i);
   newbtn.setAttribute("class", "btn btn-outline-success");
   newbtn.setAttribute("onclick", "valorbt('" + i + "','" + idbtcompra + "');"); //valorbt(btvalor,idvisorbt)
-  newbtn.textContent = "Añadir Pedido";
+  newbtn.textContent = "Añadir";
+  
   newbtn.type = "button";
   newbtn.value = i;
 
